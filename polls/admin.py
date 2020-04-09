4bbf6ca45f657bb138cdcd0e16b63cfb8b487897
Deepak G <deepak.g@krds.fr>
2020-04-09 11:04:57 +0530

    user table added

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Choice
+from .models import Question, Choice, Todo, User
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Todo)
+admin.site.register(User)
